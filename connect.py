a0a8f809bed9c6e9981d66b4aa59a5a64cca1ce2
aart <aart.vebeke@gmail.com>
2021-03-29 14:23:36 +0200

    initial commit

--- connect.py
+++ connect.py
@@ -3,7 +3,7 @@ from google.oauth2 import service_account
 
 # TODO(developer): Set key_path to the path to the service account key
 #                  file.
-key_path = "testground-97-7f85792d9910.json"
+key_path = ".json"
 
 credentials = service_account.Credentials.from_service_account_file(
     key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
