19e9e2baab4f92a6102bea3b16801b13028bdaa0
aart <aart.vebeke@gmail.com>
2021-03-29 14:17:09 +0200

    initial commit

---
+++ connect.py
@@ -0,0 +1,12 @@
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
+# TODO(developer): Set key_path to the path to the service account key
+#                  file.
+key_path = "testground-97-7f85792d9910.json"
+
+credentials = service_account.Credentials.from_service_account_file(
+    key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
+)
+
+client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
